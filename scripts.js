62446b3b6efd719a6d13061ea8a6462defb86123
federico <donky1989@gmail.com>
2018-12-13 10:08:36 +0100

    Loading Bar

--- scripts.js
+++ scripts.js
@@ -8,7 +8,11 @@ import TwoPointStorm from './js/model/TwoPointStorm.js';
 import Enemy from './js/model/Enemy.js';
 import Star from './js/model/Star.js';
 import ViewMediatorFactory from './js/view/ViewMediaFactory.js';
+
+var elem = document.getElementById("myBar");
 Globals.instance.manager.onLoad = function () {
+   var bar = document.getElementById("myProgress");
+    
     Globals.instance.environment = new Environment('Mars');
     const envController = new EnvironmentController(Globals.instance.environment);
     const factory = new ViewMediatorFactory();
@@ -18,8 +22,22 @@ Globals.instance.manager.onLoad = function () {
     Globals.instance.environment.addTurret(turret);
     const sun=new Star('sun');
     Globals.instance.environment.addStar(sun);
-}
+    bar.style.visibility = "hidden";
+    elem.style.visibility = "hidden";
 
+};
+Globals.instance.manager.onStart = function (url, itemsLoaded, itemsTotal) {
+        elem.style.visibility = "visible";
+
+}
+Globals.instance.manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+
+        let width = (itemsLoaded/itemsTotal)*100;
+        console.log(width);
+        elem.style.width = width + '%';
+        elem.innerHTML = width + '%';
+
+};
 let turret_loader = new THREE.ColladaLoader(Globals.instance.manager);
 let enemy_loader = new THREE.ColladaLoader(Globals.instance.manager);
 let ground_loader = new THREE.MTLLoader(Globals.instance.manager);
